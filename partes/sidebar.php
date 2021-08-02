d5b5cccec4072192cd95b97665f88589d3f35917
oskhar-ogg <oscarecaroc@outlook.com>
2021-08-01 20:40:19 -0400

    Update sidebar.php

--- partes/sidebar.php
+++ partes/sidebar.php
@@ -4,7 +4,7 @@
                 <h4 class="text-light font-weight-bold mb-0">UNIVERSIDAD DEL SUR</h4>
             </div>
             <div class="menu">
-                <a href="../" class="d-block text-light p-3 border-0"><i class="fas fa-tachometer-alt"></i>
+                <a href="index.php" class="d-block text-light p-3 border-0"><i class="fas fa-tachometer-alt"></i>
                     Inicio</a>
 
                 <a href="../" class="d-block text-light p-3 border-0"><i class="fas fa-users"></i>
