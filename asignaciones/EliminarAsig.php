64edf4b8eac70c6fad49bab86c301f0c69737366
Taos98 <tomasmmontecinos@gmail.com>
2021-08-04 09:15:14 -0400

    Update EliminarAsig.php

--- asignaciones/EliminarAsig.php
+++ asignaciones/EliminarAsig.php
@@ -12,7 +12,7 @@ if (isset($_POST['submit']))
     $nueva_fecha = $_POST["Nueva_Fecha"];
     $id_equipo =$_POST["IDEQUIPO"];
 
-    $query_insertar_asignacion = mysqli_query($conexion," UPDATE `asignacion` SET `fecha_vencimiento` = '$nueva_fecha' WHERE `equipo_id_equipo` = '$id_equipo' ");
+    $query_eliminar_asignacion = mysqli_query($conexion," UPDATE `asignacion` SET `fecha_vencimiento` = '$nueva_fecha' WHERE `equipo_id_equipo` = '$id_equipo' ");
 
     echo "Fecha modificada!!.";
 }
