8f1231f42db8bae30db695690b40f1be70cb5590
EduardoHernandezOUBB <Optickal095@gmail.com>
2021-08-04 04:48:05 -0400

    .

--- asignaciones/asignaciones.php
+++ asignaciones/asignaciones.php
@@ -189,8 +189,6 @@ $Consulta_asignacion = "SELECT * FROM asignacion";
       <div class="modal-content">
         <div class="modal-header">
           <h5 class="modal-title" id="exampleModalLabel">Asignar Equipo</h5>
-          <h5 class="modal-title" id="exampleModalLabel">Asignar Equipo</h5>
-          <h5 class="modal-title" id="exampleModalLabel">Asignar Equipo</h5>
           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">&times;</span>
           </button>
