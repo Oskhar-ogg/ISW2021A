f14aea20c0d1c978ee5c2f6b12150a7c40aadf77
Taos98 <tomasmmontecinos@gmail.com>
2021-08-04 07:21:58 -0400

    Update NuevaFecha.php

---
+++ asignaciones/NuevaFecha.php
@@ -0,0 +1,19 @@
+<?php 
+
+include("../conexion/conexion.php");
+include("../asignaciones/asignaciones.php");
+
+
+$Consulta_id_usuario = "SELECT id_usuario FROM usuario WHERE rut_usuario = 'Rut-Asignado'";
+
+if (isset($_POST['submit']))
+{
+    
+    $nueva_fecha = $_POST["Nueva_Fecha"];
+    $id_equipo =$_POST["IDEQUIPO"];
+
+    $query_insertar_asignacion = mysqli_query($conexion," UPDATE `asignacion` SET   ");
+
+    echo "Asignacion lista!!.";
+}
+?>
