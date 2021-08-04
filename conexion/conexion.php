6e9691320cc42a6640a3d565760a4368c22c2909
Taos98 <tomasmmontecinos@gmail.com>
2021-08-04 06:50:39 -0400

    Update conexion.php

--- conexion/conexion.php
+++ conexion/conexion.php
@@ -1,7 +1,3 @@
-<?php 
-$conexion = mysqli_connect("mysql.face.ubiobio.cl", "g1ieci2021", "g5ieci2021", "g1bd2021");
-if ($conexion->connect_error){
-    die("CONEXION FALLIDA: ");   
-}
+<?php
+$conexion = mysqli_connect("localhost", "root", "TomiMati2005", "empresaa_bd" );
 mysqli_set_charset($conexion, "utf8");
-?>
