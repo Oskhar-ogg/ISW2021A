3a73eeaa52206d1897ae666253a6219dcbb74e1e
oskhar-ogg <oscarecaroc@outlook.com>
2021-08-04 06:00:16 -0400

    Update conexion.php

--- conexion/conexion.php
+++ conexion/conexion.php
@@ -1,10 +1,7 @@
 <?php 
-/*
 $conexion = mysqli_connect("mysql.face.ubiobio.cl", "g1ieci2021", "g5ieci2021", "g1bd2021");
-
 if ($conexion->connect_error){
-    die("CONEXION FALLIDA: " . $conn->connect_error);   
+    die("CONEXION FALLIDA: ");   
 }
 mysqli_set_charset($conexion, "utf8");
 ?>
-*/
